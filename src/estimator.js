f6918b0738405668f63b95775a100753f7eba68a
Alemoh Rapheal <raphealenike@gmail.com>
2020-04-10 15:55:03 -0700

    Days, weeks and month fixed

--- src/estimator.js
+++ src/estimator.js
@@ -21,10 +21,10 @@ const covid19ImpactEstimator = (data) => {
       return Math.ceil(hospitalBeds - this.severeCasesByRequestedTime);
     },
     get casesForICUByRequestedTime() {
-      return Math.ceil(this.infectionsByRequestedTime * 0.05);
+      return Math.trunc(this.infectionsByRequestedTime * 0.05);
     },
     get casesForVentilatorsByRequestedTime() {
-      return Math.ceil(this.infectionsByRequestedTime * 0.02);
+      return Math.trunc(this.infectionsByRequestedTime * 0.02);
     },
     get dollarsInFlight() {
       return this.infectionsByRequestedTime * population * income * days;
@@ -46,11 +46,11 @@ const covid19ImpactEstimator = (data) => {
     },
 
     get casesForICUByRequestedTime() {
-      return this.infectionsByRequestedTime * 0.05;
+      return Math.trunc(this.infectionsByRequestedTime * 0.05);
     },
 
     get casesForVentilatorsByRequestedTime() {
-      return this.infectionsByRequestedTime * 0.02;
+      return Math.trunc(this.infectionsByRequestedTime * 0.02);
     },
 
     get dollarsInFlight() {
